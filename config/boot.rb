e2780372549f5ea42cec026b1e626a951857b2fd
Matt Haliski <matt@haliski.com>
2023-03-02 07:34:54 -0800

    Set up Docker Compose.

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,7 @@
+# Switch off Ruby's output buffering because it doesn't play nicely with
+# Docker Compose.
+$stout.sync = true
+
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
