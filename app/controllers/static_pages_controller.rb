ed6acb9c60815a4100721ea5b158b3f6ac6536a0
Matt Haliski <matt@haliski.com>
2023-07-21 13:38:44 -0700

    Added a homepage rather than have the default Rails app showing.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,5 @@
+# Pages that aren't tied to a model
+class StaticPagesController < ApplicationController
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "static_pages#home"
   get "welcome", to: "welcome#index"
 end
